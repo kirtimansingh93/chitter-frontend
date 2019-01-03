e2ef494c164d8cc267399e8d05251b45411a5d8d
Kirtiman Singh <kirt93@hotmail.com>
2019-01-03 17:45:39 +0000

    view peeps edited

--- js/peeps.js
+++ js/peeps.js
@@ -1,12 +1,21 @@
 const peepsContainer = document.getElementById('peeps');
 
-const ourRequest = new XMLHttpRequest();
-ourRequest.open('get', 'https://chitter-backend-api.herokuapp.com/peeps');
-ourRequest.onload = function() {
-  const resObjData = JSON.parse(this.responseText);
-  renderDataToHTML(resObjData);
+const xhr = new XMLHttpRequest();
+xhr.open('get', 'https://chitter-backend-api.herokuapp.com/peeps');
+xhr.onload = function() {
+  if (this.status == 200) {
+    try {
+      const resObjData = JSON.parse(this.responseText);
+      renderDataToHTML(resObjData);
+    } catch (e) {
+      console.warn('There was an error in the JSON. Could not parse!');
+    }
+  } else {
+      console.warn('Did not receive 200 OK from response!');
+  }
 };
-ourRequest.send();
+xhr.send();
+
 
 function renderDataToHTML(data) {
   let peepsDivHTML = '';
