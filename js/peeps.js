3eeadf197f32e26750f36ccb223bc74a172d75b0
Kirtiman Singh <kirt93@hotmail.com>
2019-01-16 20:38:54 +0000

    signIn function and signUp function

--- js/peeps.js
+++ js/peeps.js
@@ -1,7 +1,7 @@
 const peepsContainer = document.getElementById('peeps');
 
 const xhr = new XMLHttpRequest();
-xhr.open('get', 'https://chitter-backend-api.herokuapp.com/peeps');
+xhr.open('GET', 'https://chitter-backend-api.herokuapp.com/peeps', true);
 xhr.onload = function() {
   if (this.status == 200) {
     try {
@@ -16,13 +16,53 @@ xhr.onload = function() {
 };
 xhr.send();
 
-
 function renderDataToHTML(data) {
   let peepsDivHTML = '';
 
   for (let i = 0; i < data.length; i++) {
-    peepsDivHTML += '<p>' + data[i].user.handle + ': ' + data[i].body + ' (created at '+ data[i].created_at.slice(11, 16) + ' on ' + data[i].created_at.slice(0, 10) + ') </p>'
+    peepsDivHTML += `<p><strong>${data[i].user.handle}</strong>: ${data[i].body} <em>(created at ${data[i].created_at.slice(11, 16)} on ${data[i].created_at.slice(0, 10)})</em></p>`
   }
-
-  peepsContainer.insertAdjacentHTML('beforeend', peepsDivHTML)
+  document.getElementById('peeps').innerHTML = peepsDivHTML;
 }
+
+
+
+
+function signUp() {
+  const xhttp = new XMLHttpRequest();
+  xhttp.open("POST", "https://chitter-backend-api.herokuapp.com/users", true);
+  xhttp.setRequestHeader("Content-Type", "application/json");
+  let input = JSON.stringify({ "user": { "handle": "kirt", "password": "mypassword" } });
+
+  xhttp.send(input);
+}
+
+
+// const xhttp2 = new XMLHttpRequest();
+// xhttp2.open("POST", "https://chitter-backend-api.herokuapp.com/sessions", true);
+// xhttp2.setRequestHeader("Content-Type", "application/json");
+// const resObjData2 = JSON.stringify({
+//                     "session": {"handle":"kirt", 
+//                                 "password":"mypassword"
+//                               }
+//                             });
+
+// xhttp2.send(resObjData2);
+
+function signIn(username, password) {
+  var url = 'https://chitter-backend-api.herokuapp.com/sessions';
+  var data = `{ "session": { "handle": "${username}", "${password}": "mypassword" } }`;
+
+  fetch(url, {
+    method: 'POST', // or 'PUT'
+    body: JSON.stringify(data), // data can be `string` or {object}!
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }).then(res => res.json())
+    .then(response => console.log('Success:', JSON.stringify(response)))
+    .catch(error => console.error('Error:', error));
+
+}
+
+  
