5ebfee2d387b348f5698a6cf2b88b003948efec9
Kirtiman Singh <kirt93@hotmail.com>
2019-01-22 11:45:37 +0000

    Can post a peep and can log out

--- js/peeps.js
+++ js/peeps.js
@@ -1,6 +1,7 @@
 const peepsContainer = document.getElementById('peeps');
 
 function getPeeps() {
+  console.log('get peeps')
   const xhr = new XMLHttpRequest();
   xhr.open('GET', 'https://chitter-backend-api.herokuapp.com/peeps', true);
   xhr.onload = function () {
@@ -16,6 +17,11 @@ function getPeeps() {
     }
   };
   xhr.send();
+}
+
+function logOut() {
+  sessionStorage.clear()
+  location.reload()
 }
 
 
@@ -59,29 +65,48 @@ function signIn() {
   let data = { session: { handle: username, password: password } };
 
   fetch(url, {
-    method: 'POST', // or 'PUT'
-    body: JSON.stringify(data), // data can be `string` or {object}!
+    method: 'POST',
+    body: JSON.stringify(data),
     headers: {
       'Content-Type': 'application/json'
     }
   }).then(res => res.json())
-    .then(getPeeps())
-    .then(response => console.log('Success:', JSON.stringify(response)))
-    .catch(error => console.error('Error:', error));
-    
+    // .then(response => console.log('Success:', response))
+    .then(response => {
+      console.log(response);
+      sessionStorage.setItem('user_id', `${response['user_id']}`);
+      sessionStorage.setItem('session_key', `${response['session_key']}`);
+    })
+    .catch(error => console.error('Incorrect login details', error))
+    .then(getPeeps());
 }
 
-// function postPeep(peep) {
-//   let url = 'https://chitter-backend-api.herokuapp.com/peeps';
-//   let data = `{ "session": { "handle": "${username}", "${password}": "mypassword" } }`;
+function sessionKey() {
+  return sessionStorage.getItem('session_key')
+}
 
-//   fetch(url, {
-//     method: 'POST', // or 'PUT'
-//     body: JSON.stringify(data), // data can be `string` or {object}!
-//     headers: {
-//       'Content-Type': 'application/json'
-//     }
-//   }).then(res => res.json())
-//     .then(response => console.log('Success:', JSON.stringify(response)))
-//     .catch(error => console.error('Error:', error));
-// }
+
+function postPeep() {
+  let userID = parseInt(sessionStorage.getItem('user_id'));
+  let post = document.getElementById('peep').value;
+  let url2 = 'https://chitter-backend-api.herokuapp.com/peeps';
+  let data2 = { peep: { user_id:userID, body:post } };
+  // let data2 = `{ peep: { user_id:${userID}, body:${post} } }`;
+  // '{"peep": {"user_id":1, "body":"my first peep :)"}}'
+  
+  // let myHeaders = new Headers();
+  // myHeaders.append('Content-Type', 'application/json');
+  // myHeaders.append('Authorization', `Token token=${sessionKey}`);
+
+  fetch(url2, {
+    method: 'POST',
+    body: JSON.stringify(data2), 
+    headers: {
+      'Authorization': `Token token=${sessionKey()}`,
+      'Content-Type': 'application/json'
+    }
+  }).then(res => res.json())
+    .then(response => console.log('Success:', JSON.stringify(response)))
+    .catch(error => console.error('Error:', error))
+    .then(getPeeps());
+}
